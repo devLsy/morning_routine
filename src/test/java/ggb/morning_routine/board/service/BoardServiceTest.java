6514e6d74179325652bd12d795babefc7d715cbf
devlsy86 <infightersy@naver.com>
2023-07-08 10:03:00 +0900

    에러 잡자

--- src/test/java/ggb/morning_routine/board/service/BoardServiceTest.java
+++ src/test/java/ggb/morning_routine/board/service/BoardServiceTest.java
@@ -26,7 +26,13 @@ class BoardServiceTest {
         BoardVo boardVo = new BoardVo();
         boardVo.setRecordDate("20230621");
         boardVo.setBedtime("20230620 22:40");
-        boardVo.setWakeUpTime("20230621 05:00");
+        boardVo.setScheduledWakeUpTime("20230621 05:00");
+        boardVo.setWakeUpTime("20230621 08:14");
+        boardVo.setWalkYn("N");
+        boardVo.setSuccessFailureYn("N");
+        boardVo.setNote("05시에 일어났으나 알람 6시로 맞추고 다시 6시에 일어나서 알람 07시로 맞추고 이짓을 반복하다 결국 8시14분에 기상함");
+
+        boardMapper.insertBoard(boardVo);
         //when
 
         //then
